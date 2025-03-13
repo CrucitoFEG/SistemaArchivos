dca8827cd7d0a9f2028703bab16ccb40db9de0a0
festrada <festrada@192.168.1.32>
2025-03-12 21:48:40 -0600

    Inicial2

--- src/sistemaarchivos2/Nodo.java
+++ src/sistemaarchivos2/Nodo.java
@@ -1,38 +1,20 @@
 
 package sistemaarchivos2;
 
+/**
+ *
+ * @author festrada
+ */
 public class Nodo {
     String nombre;
     boolean esDirectorio;
-    Nodo izquierdo, derecho;  // Hijo izquierdo e Hijo derecho
-    String[] permisos;
-    int tamaño; // En KB
-    String fechaCreacion;
-    String fechaModificacion;
+    Nodo izquierdo;  // Hijo izquierdo
+    Nodo derecho;    // Hijo derecho
 
-    public Nodo(String nombre, boolean esDirectorio,String[] permisos, int tamaño, String fechaCreacion, String fechaModificacion) {
+    public Nodo(String nombre, boolean esDirectorio) {
         this.nombre = nombre;
         this.esDirectorio = esDirectorio;
-        this.permisos = permisos;
-        this.tamaño = tamaño;
-        this.fechaCreacion = fechaCreacion;
-        this.fechaModificacion = fechaModificacion;
         this.izquierdo = null;
         this.derecho = null;
-    }
-    
-    public boolean tienePermiso(String permiso) {
-        for (String p : permisos) {
-            if (p.equals(permiso)) return true;
-        }
-        return false;
-    }
-
-    public void cambiarPermisos(String[] nuevosPermisos) {
-        this.permisos = nuevosPermisos;
-    }
-
-    public void mostrarPermisos() {
-        System.out.println("Permisos de " + nombre + ": " + String.join(", ", permisos));
     }
 }
